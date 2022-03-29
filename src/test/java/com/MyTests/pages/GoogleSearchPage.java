cd7c4ce0b4111a11b8a5c70eff108cd30771685e
adiladilixiati <adila.dilixiati@gmail.com>
2022-03-28 23:25:59 -0700

    another commit

--- src/test/java/com/MyTests/pages/GoogleSearchPage.java
+++ src/test/java/com/MyTests/pages/GoogleSearchPage.java
@@ -1,24 +1,19 @@
 package com.MyTests.pages;
 
 import com.MyTests.utilities.Driver;
-import com.MyTests.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class WikiSearchPage {
+public class GoogleSearchPage {
 
-    public WikiSearchPage(){
+    //create constructor
+    // initialize the driver instance and this class' instance using PageFactory.initElements
+    public GoogleSearchPage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    @FindBy(id = "searchInput")
+    //We can start locating web elements using @FindBy annotation
+    @FindBy(name = "q")
     public WebElement searchBox;
-
-    @FindBy(xpath = "//button[@type='submit']")
-    public WebElement searchButton;
-
-    @FindBy(id = "firstHeading")
-    public WebElement mainHeader;
-
 }
