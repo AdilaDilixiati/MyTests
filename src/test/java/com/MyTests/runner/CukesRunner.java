830119a78a54242442bedb9bb91ed7bab7d9d396
adiladilixiati <adila.dilixiati@gmail.com>
2022-03-30 22:25:10 -0700

    updated jenkinsfile

--- src/test/java/com/MyTests/runner/CukesRunner.java
+++ src/test/java/com/MyTests/runner/CukesRunner.java
@@ -7,10 +7,9 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {
                 "json:target/cucumber.json",
-                "html:target/cucumber-report.html",
+                "html:target/cucumber/report.html",
+                "junit:target/junit/junit-report.xml",
                 "rerun:target/rerun.txt",
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
-
         },
         features = "src/test/resources/features",
         glue = "com/MyTests/step_definitions",
